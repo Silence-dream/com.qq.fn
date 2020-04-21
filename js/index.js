117bcda26507d9fa0673e2effd4c6dba3984af77
ℳ๓₯㎕白月初 <42824008+Silence-dream@users.noreply.github.com>
2020-04-21 11:57:08 +0800

    主页完成

--- js/index.js
+++ js/index.js
@@ -188,4 +188,314 @@ $(function () {
     }, function () {
         $(this).removeAttr("style");
     })
+
+
+
+    /* 最新视频 */
+    $("#video .head li").on("mouseenter", function () {
+        // 伪元素添加
+        $(this).addClass("cur cur1").siblings().removeClass("cur cur1");
+        let index = $(this).index();
+        $("#video .content-list").empty();
+        if (index == 0) {
+            $("#video .content-list").append(`
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img1.jpg" alt="">
+            </div>
+            <p class="name">堡垒之夜 x 死侍 联动开启！全新宣传片首发！</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img2.jpg" alt="">
+            </div>
+            <p class="name">谁能获得最终胜利！堡垒之夜间谍模式来袭</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img3.jpg" alt="">
+            </div>
+            <p class="name">堡垒第二夜第二赛季官方宣传片：特工对决</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img4.jpg" alt="">
+            </div>
+            <p class="name">堡垒第二夜第二赛季英雄季卡宣传片：特工对决</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img5.jpg" alt="">
+            </div>
+            <p class="name">堡垒第二夜第二赛季官方宣传片：特工对决</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img6.jpg" alt="">
+            </div>
+            <p class="name">V11.40 武器升级平台更新 转化功能登场</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img7.jpg" alt="">
+            </div>
+            <p class="name">星球大战联动，全新武器光剑到底有多酷？</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img8.jpg" alt="">
+            </div>
+            <p class="name">天行者崛起！星球大战联动大事件全程直播</p>
+            <a href="javascript:;"></a>
+        </li>
+            `)
+        } else if (index == 1) {
+            $("#video .content-list").append(`
+            <li>
+            <div class="pic-box">
+                <img src="./images/video-img1.jpg" alt="">
+            </div>
+            <p class="name">堡垒之夜 x 死侍 联动开启！全新宣传片首发！</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img2.jpg" alt="">
+            </div>
+            <p class="name">谁能获得最终胜利！堡垒之夜间谍模式来袭</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img3.jpg" alt="">
+            </div>
+            <p class="name">堡垒第二夜第二赛季官方宣传片：特工对决</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img4.jpg" alt="">
+            </div>
+            <p class="name">堡垒第二夜第二赛季英雄季卡宣传片：特工对决</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img5.jpg" alt="">
+            </div>
+            <p class="name">堡垒第二夜第二赛季官方宣传片：特工对决</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img6.jpg" alt="">
+            </div>
+            <p class="name">V11.40 武器升级平台更新 转化功能登场</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img7.jpg" alt="">
+            </div>
+            <p class="name">星球大战联动，全新武器光剑到底有多酷？</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img8.jpg" alt="">
+            </div>
+            <p class="name">天行者崛起！星球大战联动大事件全程直播</p>
+            <a href="javascript:;"></a>
+        </li>
+            `)
+        } else if (index == 2) {
+            $("#video .content-list").append(`
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img9.jpg" alt="">
+            </div>
+            <p class="name">堡垒哥：最隐蔽的地方，100个人都找不到</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img10.jpg" alt="">
+            </div>
+            <p class="name">堡垒哥：这样拜年压岁钱更多，当红男团包子兄弟共贺新春！</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img11.jpg" alt="">
+            </div>
+            <p class="name">堡垒哥：星球大战凌晨3点大彩蛋，1000外星人入侵堡垒大陆！</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img12.jpg" alt="">
+            </div>
+            <p class="name">堡垒哥：空中发现巨型战舰，外星人入侵堡垒大地！新联动星球大战</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img13.jpg" alt="">
+            </div>
+            <p class="name">堡垒哥：100人使用裂缝会怎样？盘点游戏十大改动</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img14.jpg" alt="">
+            </div>
+            <p class="name">堡垒哥：全球玩家被黑洞吞噬！史上最大彩蛋</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img15.jpg" alt="">
+            </div>
+            <p class="name">堡垒哥：蔡徐坤客串漫威DC大片？演技全程被蝙蝠侠无视</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img16.jpg" alt="">
+            </div>
+            <p class="name">三门来了：这次没有攻略，只有满满的祝福送给堡垒之夜周年庆！</p>
+            <a href="javascript:;"></a>
+        </li>
+            `)
+        } else if (index == 3) {
+            $("#video .content-list").append(`
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img17.jpg" alt="">
+            </div>
+            <p class="name">【艾登小技巧】100秒学会侧跳！有手就行！</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img18.jpg" alt="">
+            </div>
+            <p class="name">【三门充电站】N改板方式，哪种最适合你？新手必看</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img19.jpg" alt="">
+            </div>
+            <p class="name">【堡垒模范生】发生在未来，第九赛季地图变化</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img20.jpg" alt="">
+            </div>
+            <p class="name">【三门充电站】岩浆里装备拿不到？小技巧Get！</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img21.jpg" alt="">
+            </div>
+            <p class="name">【堡垒模范生】第八赛季大事件第一视角</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img22.jpg" alt="">
+            </div>
+            <p class="name">【三门充电站】职业哥都用的滑轮小技巧！你会几个？</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img23.jpg" alt="">
+            </div>
+            <p class="name">【堡垒模范生】符文显现！第八赛季大事件相关</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img24.jpg" alt="">
+            </div>
+            <p class="name">【艾登小技巧】不搭板玩家的福音！猛男架枪位</p>
+            <a href="javascript:;"></a>
+        </li>
+            `)
+        } else if (index == 4) {
+            $("#video .content-list").append(`
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img25.jpg" alt="">
+            </div>
+            <p class="name">世界杯冠军：Bugha的战斗巴士之旅</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img26.jpg" alt="">
+            </div>
+            <p class="name">赏金赛NA东部总决赛第5场[最终结果]</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img27.jpg" alt="">
+            </div>
+            <p class="name">赏金赛冠军系列赛决赛-NA东部总决赛第5场</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img28.jpg" alt="">
+            </div>
+            <p class="name">赏金赛冠军系列赛决赛-NA东部总决赛第4场</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img29.jpg" alt="">
+            </div>
+            <p class="name">赏金赛冠军系列赛决赛-NA东部总决赛第3场</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img30.jpg" alt="">
+            </div>
+            <p class="name">赏金赛冠军系列赛决赛-NA东部总决赛第2场</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img31.jpg" alt="">
+            </div>
+            <p class="name">赏金赛冠军系列赛决赛-NA东部总决赛第1场</p>
+            <a href="javascript:;"></a>
+        </li>
+        <li>
+            <div class="pic-box">
+                <img src="./images/video-img32.jpg" alt="">
+            </div>
+            <p class="name">赏金赛四连冠系列赛第十季总决赛集锦</p>
+            <a href="javascript:;"></a>
+        </li>
+            `)
+        }
+    })
+
+
+    /* 控制背景图大小 */
+    $(".bg").css("width", $("html,body").width())
 });
